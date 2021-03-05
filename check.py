2a8151bb4538c1a7c2944211773c9dbb4621f2a8
Marcelo Salazar <marcelo.salazar@arm.com>
2021-03-05 16:30:57 +0000

    First version of script

--- check.py
+++ check.py
@@ -1,9 +1,9 @@
-# Simple script to compile an app with multiple targets/toolchains
+# Simple script to generate a list of targets and features
 
 # Workflow
 # 1. Read from targets.json
-# 2. Compile app for all targets, for a given toolchain
-# 3. Generate report
+# 2. Create dict targets and features
+# 3. Print list: screen, txt or csv
 
 import requests
 import os
@@ -17,123 +17,97 @@ from datetime import datetime
 from argparse import ArgumentParser
 from prettytable import PrettyTable
 
+data = []
 
-def get_targets():
-    # Read targets from targets.json and returns list
-
-    file_name = "./mbed-os/targets/targets.json"
-
+def read_targets_json():
+    global data
+    
+    file_name = "../mbed-os/targets/targets.json"  
     with open(file_name) as json_file:
-        data = json.load(json_file)
+        data = json.load(json_file) 
 
-    targets = []
+def is_public(target):    
+    if 'public' in data[target] and data[target]['public'] == False:
+        return False
+    else:
+        return True
+
+# get features and returns dict
+# { feature1='y', feature3='y', ...}
+def get_features(target):
+
+    features = dict()
+
+    if 'inherits' in data[target]:
+        sub_target = data[target]['inherits']
+        for i in sub_target:
+            features.update(get_features(i))
+
+    if 'device_has' in data[target]:
+        for x in data[target]['device_has']:
+            new_dict = dict()
+            new_dict[x]='y'
+            features.update(new_dict)
+           
+    return(features)
+            
+def get_targets_data():
+    # Read targets from targets.json and returns dict with features
+
+    targets = dict()
     for i in data:
-        if 'public' in data[i] and data[i]['public'] == False:
+        if is_public(i) == False:
             continue
-        targets.append(i)
-
+        
+        features = get_features(i)  
+        targets[i]=features
+ 
     return(targets)
 
-def compile_app(toolchain, target):
+def print_table(target_list):
 
-    compile_options = " -v"
-    command = "mbed compile -t " + str(toolchain) + \
-                " -m " + str(target) + str(compile_options)
-                
-    print("command: " + command)
-
-    # Save current path        
-    #top_path = os.getcwd()
-    #print("path: " + top_path)
-    # Change to library folder before compiling
-    #temp = top_path + '/' + dft_temp_dir + self.apps[app_n]["local_dir"] + '/'
-    #os.chdir(temp)
-
-
-    try:
-        output = subprocess.check_call(command , shell=True, stderr=subprocess.STDOUT)
-        return "PASS" # compile successful
-
-    except Exception:
-        print("mbed compile error\n")
-        print(subprocess.STDOUT)
-        return "FAIL" # compile failed
-
-def print_table(test_result, toolchains):
-
-    table_header = ["Target"] + toolchains
+    # get list of features
+    all_features = []
+    for i in target_list:
+        features = target_list[i].keys()
+        for x in features:
+            if x not in all_features:
+               all_features.append(x)
+               
+    table_header = ["Target"] + all_features
     table = PrettyTable(table_header)
 
     table.align['Target'] = 'l'
 
-    for m in test_result:
-        table.add_row([str(m)] + [test_result[m][a] for a in toolchains])
+    for i in target_list:
+        temp_features=[]
+        for x in all_features:
+            if x in target_list[i].keys():
+                temp_features.append('y')
+            else:
+                temp_features.append(' ')
+        
+        row = [i] + temp_features
+        table.add_row(row)
 
     print(table)
-
-def save_results(test_result, toolchains):
-
-    yr = datetime.now().year
-    mo = datetime.now().month
-    day = datetime.now().day
-    hr = datetime.now().hour
-    minu = datetime.now().minute
     
-    filename = "results_" + str(yr) + str(mo) + str(day) + str(hr) + str(minu) + ".txt"
-    filew = open(filename,"w") 
-
-    line = "Target " + str(' '.join(toolchains)) + "\n"
-    filew.writelines(line)
-
-    for m in test_result:
-        line = str(m) + " " + str(' '.join([test_result[m][a] for a in toolchains])) + "\n"
-        filew.writelines(line)
-
-    filew.close() 
-
+    
 def main():
     global args
 
     # Parser handling
-    parser = ArgumentParser(description="Automation script to compile apps for targets/toolchain")
+    parser = ArgumentParser(description="Script to get list of targets/features")
 
     parser.add_argument(
         '-m', '--target', dest='target',
         help='Target name', required=False)
 
-    parser.add_argument(
-        '-t', '--toolchain', dest='toolchain',
-        help='Toolchain name (ARM, GCC_ARM)', required=False)
-
     args = parser.parse_args()
-
-    # Optional target
-    if args.target:
-        targets = str(args.target).split(",") #  compile only for a target, otherwise all targets
-    else:
-        # Read from targets.json
-        targets = get_targets()
-
-    # Optional toolchain:
-    if args.toolchain:
-        toolchains = str(args.toolchain).split(",")
-    else:
-        toolchains = ["ARM", "GCC_ARM"]
-        
-    test_result = {}
-
-    # Compile app for all targets, for a given toolchain
-    for m in targets:
-        temp = dict()
-        for t in toolchains:
-            result = compile_app(t, m)
-            temp[t] = result
-        test_result[m] = temp
-
-    # Generate report
-    # Default format PrettyTable, TODO Github markdown
-    save_results(test_result, toolchains)
-    print_table(test_result, toolchains)
+   
+    read_targets_json()
+    target_list = get_targets_data()
+    print_table(target_list)
 
     exit(0)
 
